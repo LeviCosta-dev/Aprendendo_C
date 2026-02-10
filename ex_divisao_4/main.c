9737dc4450786a0705472b3fad4205a96902d873
LeviCosta-dev <leviccosta.dev@gmail.com>
2026-02-10 17:39:07 -0300

    Alterei um exercício

--- ex_divisao_4/main.c
+++ ex_divisao_4/main.c
@@ -4,15 +4,17 @@
 
 int main(void)
 {
-    int i, n;
+    int i, n, m;
 
     printf("\nDigite um numero\n");
     scanf("%d", &n);
+    printf("\nDigite outro numero para descobrir seus multiplos\n");
+    scanf("%d", &m);
 
-    printf("\nOs multiplos de 4 entre 1 e %d sao:\n", n);
+    printf("\nOs multiplos de %d entre 1 e %d sao:\n", m, n);
     for (i = 1; i <= n; i++)
     {
-        if (i % 4 == 0)
+        if (i % m == 0)
         {
             printf("%d\n", i);
         }
